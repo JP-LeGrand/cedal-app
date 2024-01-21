66f232e90cf21680478610d0042f63605759459a
ENTELECT\joseph.legrand <joseph.legrand@entelect.co.za>
2024-01-21 23:53:39 +0200

    first commit

---
+++ public/images/cedal/Images.jsx
@@ -0,0 +1,19 @@
+/*Images downloaded from https://unsplash.com/*/
+
+/*Import*/
+import srcVolunteers from "./Volunteers.jpg";
+import srcEducation from "./Education.jpg";
+import srcGirls from "./Girls.jpg";
+import srcHealth from "./Health.jpg";
+import srcSocial from "./Social.jpg";
+import srcDonate from "./Donate.jpg";
+import srcCedalLogo from "./CedalLogo.png";
+
+/*Export*/
+export const ImageVolunteers = srcVolunteers;
+export const ImageEducation = srcEducation;
+export const ImageGirls = srcGirls;
+export const ImageHealth = srcHealth;
+export const ImageSocial = srcSocial;
+export const ImageDonate = srcDonate;
+export const CedalLogo = srcCedalLogo;
